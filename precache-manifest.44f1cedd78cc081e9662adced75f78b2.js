46996b44f76e08520a014645f1452100643c0119
techiemanish <manishtiwari627@gmail.com>
2021-12-11 01:08:13 +0530

    Updates

---
+++ precache-manifest.44f1cedd78cc081e9662adced75f78b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ab22a81f4d432cf2c3883a132fac85",
+    "url": "/tempreature-converter/index.html"
+  },
+  {
+    "revision": "c5d99b8494eb35d0678c",
+    "url": "/tempreature-converter/static/css/main.04b24d7a.chunk.css"
+  },
+  {
+    "revision": "a36002ab42188af876e8",
+    "url": "/tempreature-converter/static/js/2.b33bb1d0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/tempreature-converter/static/js/2.b33bb1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef8ee8c7170c73311bb",
+    "url": "/tempreature-converter/static/js/3.225ca3fc.chunk.js"
+  },
+  {
+    "revision": "c5d99b8494eb35d0678c",
+    "url": "/tempreature-converter/static/js/main.7d234267.chunk.js"
+  },
+  {
+    "revision": "13143e06d32a2515c256",
+    "url": "/tempreature-converter/static/js/runtime-main.bf56d141.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/tempreature-converter/static/media/logo.06e73328.svg"
+  }
+]);
